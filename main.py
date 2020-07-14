199fa552aa4c62e88fb235e406e0dc1b216ad882
MokkoFm <mokko2070@mail.ru>
2020-07-15 00:52:59 +0300

    edit readme.md, add pathlib, change incorrect names of variables

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ from instabot import Bot
 from os import listdir
 from dotenv import load_dotenv
 import os.path
+from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-def download_picture():
-    filename = os.path.join(BASE_DIR, 'space-pictures', 'images', 'hubble.jpg')
-    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+def download_picture(url):
+    filename = Path('images', 'hubble.jpg')
     response = requests.get(url)
     response.raise_for_status()
     with open(filename, 'wb') as file:
@@ -30,7 +28,7 @@ def fetch_spacex_last_launch():
     spacex_pictures = response.json()['links']['flickr_images']
 
     for picture_number, picture in enumerate(spacex_pictures):
-        file_with_picture = os.path.join(BASE_DIR, 'space-pictures', 'images', 'spacex{}.jpg'.format(picture_number))
+        file_with_picture = Path('images', 'spacex{}.jpg'.format(picture_number))
         response = requests.get(spacex_pictures[picture_number])
 
         with open(file_with_picture, 'wb') as file:
@@ -47,7 +45,7 @@ def download_hubble_pictures(id_image):
         link = picture['file_url']
         url = "https:" + link
         split_pictures = link.split('.')
-        filename = os.path.join(BASE_DIR, 'space-pictures', 'images', 'hubble{}.{}'.format(id_image, split_pictures[-1]))
+        filename = Path('images', 'hubble{}.{}'.format(id_image, split_pictures[-1]))
         response = requests.get(url)
         response.raise_for_status()
 
@@ -63,7 +61,7 @@ def download_hubble_collection(collection):
 
     for picture in hubble_collection:
         image_id = picture['id']
-        url = "http://hubblesite.org/api/v3/image/" + str(image_id)
+        url = f"http://hubblesite.org/api/v3/image/{image_id}"
         response = requests.get(url)
         response.raise_for_status()
         hubble_collection = response.json()['image_files']
@@ -71,7 +69,7 @@ def download_hubble_collection(collection):
         for picture in hubble_collection:
             link = picture['file_url']
             url = "https:" + link
-            filename = os.path.join(BASE_DIR, 'space-pictures', 'collection', 'hubble-{}-{}.jpg'.format(collection, image_id))
+            filename = Path('collection', 'hubble-{}-{}.jpg'.format(collection, image_id))
             response = requests.get(url)
             response.raise_for_status()
 
@@ -80,10 +78,11 @@ def download_hubble_collection(collection):
 
 
 def main():
+    os.chdir("C:/Users/mokko/Desktop/space-bot/space-pictures")
     load_dotenv()
-    download_picture()
+    download_picture("https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg")
     download_hubble_pictures("1")
-    download_hubble_collection("spacecraft")
+    #download_hubble_collection("spacecraft")
     fetch_spacex_last_launch()
 
     insta_login = os.getenv("LOGIN")
@@ -91,21 +90,22 @@ def main():
     bot = Bot()
     bot.login(username=insta_login, password=insta_password)
 
-    folder = []
+    pictures_to_instagram = []
 
-    for i in os.walk(os.path.join(BASE_DIR, 'space-pictures', 'images')):
-        folder.append(i)
+    for file_name in os.walk(Path('images')):
+        pictures_to_instagram.append(file_name)
 
-    for i in os.walk(os.path.join(BASE_DIR, 'space-pictures', 'collection')):
-        folder.append(i)
+    for file_name in os.walk(Path('collection')):
+        pictures_to_instagram.append(file_name)
 
-    for address, dirs, files in folder:
-        for file in files:
-            instagram_image = Image.open(address+'/'+file)
+    for address, dirs, file_names in pictures_to_instagram:
+        for file_name in file_names:
+            instagram_image = Image.open(os.path.join(address, file_name))
             instagram_image.thumbnail((1080, 1080))
-            instagram_image.save(address+'/'+file)
-            bot.upload_photo(address+'/'+file)
+            instagram_image.save(os.path.join(address, file_name))
+            bot.upload_photo(os.path.join(address, file_name))
 
 
 if __name__ == '__main__':
     main()
+
--- readme.md
+++ readme.md
@@ -18,6 +18,19 @@ Then use `pip` (or `pip3`, if there is a conflict with Python2) to install depen
 pip install -r requirements.txt
 ```
 
+After that you can start:
+```
+python main.py
+```
+After downloading pictures to folders (collections, images) you will see:
+```
+2020-07-15 00:49:30,268 - INFO - LOGIN FLOW! Just logged-in: False
+2020-07-15 00:49:46,154 - INFO - Logged-in successfully as 'python_project_space_pictures'!
+FOUND: w:1080 h:811 r:1.3316892725030827
+2020-07-15 00:50:07,515 - INFO - Photo 'images\hubble.jpg' is uploaded
+```
+If you see that it works successfully. 
+
 ### Project Goals
 
 The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
